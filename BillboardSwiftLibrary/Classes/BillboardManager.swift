72692023109e3424a7c024961bf8afd591523a70
Fitzgerald Afful <fitzafful@gmail.com>
2018-11-16 21:53:13 +0000

    BillboardManagerProtocol

--- BillboardSwiftLibrary/Classes/BillboardManager.swift
+++ BillboardSwiftLibrary/Classes/BillboardManager.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-public typealias GetSingleChartCompletionHandler = (_ response: Result<ChartData>) -> Void
+public typealias GetChartCompletionHandler = (_ response: Result<ChartData>) -> Void
+
+enum ChartType: String {
+	case hot100 = "hot-100"
+}
 
 protocol BillboardManagerProtocol {
-	//Date in String, 
-	func getChart(date: String, completionHandler: @escaping GetChartCompletionHandler)
+	//Date in String,
+	func getChart(chartType: ChartType, date: String, completionHandler: @escaping GetChartCompletionHandler)
+	
+	//Date in String,
+	func getChart(chartType: ChartType, day: Int, month: Int, year: Int, completionHandler: @escaping GetChartCompletionHandler)
 }
