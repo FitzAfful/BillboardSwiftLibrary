b8c8d0dddf91bd52c912fe72ff42fdba2ee909fb
Fitzgerald Afful <fitzafful@gmail.com>
2018-11-21 23:52:38 +0000

    SPM Builds for BillboardBot

--- Package.swift
+++ Package.swift
@@ -5,9 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "BillboardSwiftLibrary",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+    products: [ .library(
             name: "BillboardSwiftLibrary",
             targets: ["BillboardSwiftLibrary"]),
     ],
